cc882e6aaf3f8ed960a0d2414eb76d03b5f0cd66
Tony Baldessari <tonybalde@hotmail.com>
2022-06-28 10:18:25 -0300

    Ejercicio 2 de JavaScript de OB

---
+++ ejer2.js
@@ -0,0 +1,18 @@
+
+
+
+const libroFav = {
+    titulo: "El Ocho",
+    autor: "Katherine Neville",
+    fecha: 2004,
+    url: "www.edicionesb.es"
+}
+
+const fechaNac = new Date("June 03 1979");
+var eresDesarrollador = true;
+
+var lista = ["Tony", 43, eresDesarrollador, fechaNac, libroFav];
+
+
+console.log(lista);
+
